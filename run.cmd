8bf72c0342bdb880a294021fb3d1d6abac7fbf5c
Michael Moser <michael.moser@freesurf.ch>
2024-07-13 17:59:47 +0200

    switching to Java v21

--- run.cmd
+++ run.cmd
@@ -4,7 +4,7 @@ rem note: adapt to the location where the KISMON jar and other files are located
 set KISMON_HOME=U:\Documents\eclipse\workspace_Vaadin\KIS-Monitoring
 cd %KISMON_HOME%
 
-set JAVA_HOME=C:\Program Files\Java\jdk-17.0
+set JAVA_HOME=C:\Program Files\Java\jdk-21.0
 echo running KISMON using %JAVA_HOME%:
 set KISMON_JAR=kis-monitoring-1.1-SNAPSHOT.jar
 
@@ -12,9 +12,9 @@ rem we need the properties file and a few others HERE:
 echo copying required resources to target:
 for %%f in (application.properties banner.txt logback.xml) do copy src\main\resources\%%f target\
 for %%f in (logback-test.xml) do copy src\test\resources\%%f target\
-for %%f in (*.p12) do copy %%f target\
+for %%f in (*.p12) do copy src\main\resources\%%f target\
 rem disabled - taking config from parent dir to avoid overwriting possibly adapted config at each startup
-rem for %%f in (*.kmc) do copy %%f target\
+for %%f in (*.kmc) do copy %%f target\
 rem in case of TLS handshake issues you can enable jSSLKeyLog to be able to read and record TLS-messages using WireShark to analyze issues
 rem TLS logging utility:
 rem for %%f in (jSSLKeyLog.jar) do copy %%f target\
