bad5c492307d922946ed24a04caf86322fb0cf57
Michael Moser <17732576+mmoser18@users.noreply.github.com>
2025-06-03 18:57:50 +0200

    Delete cleanout-repo.cmd

--- cleanout-repo.cmd
+++
@@ -1,5 +0,0 @@
-git filter-repo --path "*/ksta.kmc" --path "*/zhservices.kmc" --path "*/frobi.kmc" --invert-paths --force
-git filter-repo --path "ELCA_certificates/*" --invert-paths --force
-git filter-repo --path "old_running_version/*" --path "old_running_version" --invert-paths --force
-git filter-repo --path "running_version/*.jar" --invert-paths --force
-pause
