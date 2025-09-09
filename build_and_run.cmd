e0854731d996062ae57ad2832f7b26c820d68eb6
Michael Moser <17732576+mmoser18@users.noreply.github.com>
2025-09-09 23:00:24 +0200

    replaced `pwd` by `cd`

--- build_and_run.cmd
+++ build_and_run.cmd
@@ -2,8 +2,13 @@
 cd U:\Documents\eclipse\workspace_Vaadin\KIS-Monitoring
 call build_and_package.cmd
 
-set wd=`pwd`
+rem mimic: set wd=`cd`
+FOR /F "tokens=*" %%g IN ('cd') do (SET wd=%%g)
+
 call run.cmd
+
+echo switching back to '%wd%':
+cd "%wd%"
+cd
 pause
-cd wd
 goto begin
