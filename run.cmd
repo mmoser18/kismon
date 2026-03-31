1d7cd9e316e7dd2039fe54f2d63358987b759f6b
Michael Moser <17732576+mmoser18@users.noreply.github.com>
2026-03-31 15:36:11 +0200

    more comments and adjustment for log-config file

--- run.cmd
+++ run.cmd
@@ -4,33 +4,36 @@ rem note: adapt to the location where the KISMON jar and other files are located
 set KISMON_HOME=U:\Documents\eclipse\workspace_Vaadin\KIS-Monitoring
 cd %KISMON_HOME%
 
-set JAVA_HOME=C:\Program Files\Java\jdk-21.0
+set JAVA_HOME=C:\Program Files\Java\jdk-24.0
 echo running KISMON using %JAVA_HOME%:
 set KISMON_JAR=kis-monitoring-1.1-SNAPSHOT.jar
 
 rem we need the properties file and a few others HERE:
 echo copying required resources to target:
-for %%f in (application.properties banner.txt logback.xml) do copy src\main\resources\%%f target\
+for %%f in (application.properties banner.txt logback.xml) do echo %%f: && copy "src\main\resources\%%f" target\
 rem what was that intended for? It caused logback to always use the logback-test.xml which drove me nuts!
 rem for %%f in (logback-test.xml) do copy src\test\resources\%%f target\
-for %%f in (*.p12) do copy src\main\resources\%%f target\
-rem disabled - taking config from parent dir to avoid overwriting possibly adapted config at each startup
-for %%f in (*.kmc) do copy %%f target\
+echo copying certificates to target:
+for %%f in (*.p12) do echo %%f: && copy "src\main\resources\%%f" target\
+echo copying config file(s) to target:
+rem taking config from parent dir to avoid overwriting possibly adapted config at each startup
+for %%f in (*.kmc) do echo %%f: && copy "%%f" target\
 
 rem in case of TLS handshake issues you can enable jSSLKeyLog to be able to read and record TLS-messages using WireShark to analyze issues
 rem TLS logging utility:
 rem for %%f in (jSSLKeyLog.jar) do copy %%f target\
 
 cd target
+echo starting in directory: 
 pwd
 
 rem useful Java options (in case of handshake issues):
 rem "-Djavax.net.debug=ssl:handshake" 
 
-
 rem standard version - with config file names (with path) as args - e.g.:
 rem set configFile=test.kmc
 set configFile=home.kmc
+echo using config-file "%KISMON_HOME%\%configFile%":
 "%JAVA_HOME%\bin\java" -cp . -jar "%KISMON_JAR%" "%KISMON_HOME%\%configFile%"
 
 rem starting without args --> will use the config file specified in application.properties:
